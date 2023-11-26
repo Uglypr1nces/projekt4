ba5e0a6476ab7bfffe630b001775ffa5729a4a70
uglyprincess <112811446+Uglypr1nces@users.noreply.github.com>
2023-11-26 19:50:57 +0100

    Add files via upload

---
+++ projekt4/Login.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace projekt4
+{
+    public partial class form1 : Form
+    {
+        public form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "bob" && txtpassword.Text == "123")
+            {
+                ToDoApp mainform= new ToDoApp();
+                mainform.Show();
+                this.Hide();
+
+            }
+
+            else
+            {
+                MessageBox.Show("The User name or password you entered is incorrect, try again ");
+                txtUserName.Clear();
+                txtpassword.Clear();
+                txtUserName.Focus();
+            }
+
+        }
+
+        private void label2_Click_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
